10aada853913bf4fa5bef08bc3e1f6833f2260d6
firzywijatmiko <emailkamu@example.com>
2025-10-24 15:26:43 +0700

    bikin text dan font

--- app/src/main/java/com/example/projek4/UIpertama.kt
+++ app/src/main/java/com/example/projek4/UIpertama.kt
@@ -1,9 +1,12 @@
 package com.example.projek4
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.columnMeasurePolicy
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeCompilerApi
@@ -30,4 +33,8 @@ fun Activitaspertama(modifier: Modifier){
             text = stringResource(Id = R.string.univ),
             fontSize = 18.sp
         )
-        Spacer()
+        Spacer(Modifier = Modifier.height(height = 25.dp))
+        Card (
+            modifier = modifier
+
+        )
